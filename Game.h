811fa5027aab6171cf04362543d8b25f687cacaf
eLQeR <131271125+eLQeR@users.noreply.github.com>
2023-04-20 12:43:00 +0300

    Вивів скіппери в клас, прописав гетери для очків, добавив функції черги гравців і функцію кінця гри

--- Game.h
+++ Game.h
@@ -16,17 +16,23 @@ private:
                         {'\\','\\','\\','\\','\\'},
                         {'\\','\\','\\','\\','\\'},
                         {'\\','\\','\\','\\','\\'},};
+    bool skipper1 = true;
+    bool skipper2 = true;
+public:
+    int getPointsUser() const;
+    int getPointsComp() const;
 
 public:
 
     Game();
     void word(char (*table)[5]); //take random word from files with words
     void show_table(char (*table)[5]); //display table into console
+    void user1Turn (); //turns of users
+    void user2Turn (); //turns of users
+    void gameOver(); // display the result of game
     void play1(char (*table)[5]); //console user interface turn
     void play2(char (*table)[5]); //console user interface turn for 2 users
     void menu(char (*table)[5]); //console user interface menu
-
-
 };
 
 
